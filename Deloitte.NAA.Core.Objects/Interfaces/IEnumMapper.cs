767f3af5b9c54c09831fd9af7704cda0b7f44157
Huw Parry <huparry@deloitte.co.uk>
2022-06-01 17:20:03 +0100

    Added enum mapper interface. Added risk rating mapper.

---
+++ Deloitte.NAA.Core.Objects/Interfaces/IEnumMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deloitte.NAA.Core.Objects.Interfaces
+{
+    public interface IEnumMapper<TEnumOne, TEnumTwo>
+        where TEnumOne : struct, IConvertible
+        where TEnumTwo : struct, IConvertible
+    {
+        TEnumTwo MapTo(TEnumOne enumOne);
+
+        TEnumOne MapTo(TEnumTwo enumTwo);
+    }
+}
---
+++ Deloitte.NAA.Core.Objects/Mappers/RiskRatingMapper.cs
@@ -0,0 +1,26 @@
+﻿using Deloitte.NAA.API.Models.Common.Enums;
+using Deloitte.NAA.Core.Objects.DTO.Enums;
+using Deloitte.NAA.Core.Objects.Interfaces;
+using System;
+
+namespace Deloitte.NAA.Core.Objects.Mappers
+{
+    public class RiskRatingMapper : IEnumMapper<RiskRating, RiskRatingDto>
+    {
+        public RiskRatingDto MapTo(RiskRating enumOne) => enumOne switch
+        {
+            RiskRating.Mgtn => RiskRatingDto.Mgtn,
+            RiskRating.Gtn => RiskRatingDto.Gtn,
+            RiskRating.Normal => RiskRatingDto.Normal,
+            _ => throw new ArgumentOutOfRangeException(nameof(enumOne), $"Not expected value: {enumOne}")
+        };
+
+        public RiskRating MapTo(RiskRatingDto enumTwo) => enumTwo switch
+        {
+            RiskRatingDto.Mgtn => RiskRating.Mgtn,
+            RiskRatingDto.Gtn => RiskRating.Gtn,
+            RiskRatingDto.Normal => RiskRating.Normal,
+            _ => throw new ArgumentOutOfRangeException(nameof(enumTwo), $"Not expected value: {enumTwo}")
+        };
+    }
+}
