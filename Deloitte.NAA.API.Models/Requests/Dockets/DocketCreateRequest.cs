6ea482f285ddd51b675e4ebd4cb8b3dcad049e80
Huw Parry <huparry@deloitte.co.uk>
2022-06-07 17:37:59 +0100

    Remove and sort usings.

--- Deloitte.NAA.API.Models/Requests/Dockets/DocketCreateRequest.cs
+++ Deloitte.NAA.API.Models/Requests/Dockets/DocketCreateRequest.cs
@@ -1,11 +1,6 @@
 ﻿using Deloitte.NAA.API.Models.Common;
 using Deloitte.NAA.API.Models.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Deloitte.NAA.API.Models.Requests.Dockets
 {
