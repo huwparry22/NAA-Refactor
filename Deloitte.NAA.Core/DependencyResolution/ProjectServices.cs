fc55ebfc487b06b3305e9b3cced5bf0285caf7a1
Huw Parry <huparry@deloitte.co.uk>
2022-07-06 15:45:02 +0100

    Removed DTO concept and associated unit tests.

--- Deloitte.NAA.Core/DependencyResolution/ProjectServices.cs
+++ Deloitte.NAA.Core/DependencyResolution/ProjectServices.cs
@@ -1,20 +1,10 @@
-﻿using Deloitte.NAA.API.Models.Common;
-using Deloitte.NAA.API.Models.Common.Enums;
-using Deloitte.NAA.Core.Interfaces;
-using Deloitte.NAA.Core.Mappers;
-using Deloitte.NAA.Core.Objects.DTO.Common;
-using Deloitte.NAA.Core.Objects.DTO.Enums;
-
-namespace Microsoft.Extensions.DependencyInjection
+﻿namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ProjectServices
     {
         public static void RegisterCoreServices(this IServiceCollection services)
         {
-            services.AddTransient<IMapper<Docket, DocketDto>, DocketMapper>();
-            services.AddTransient<IMapper<Client, ClientDto>, ClientMapper>();
-
-            services.AddTransient<IEnumMapper<RiskRating, RiskRatingDto>, RiskRatingMapper>();
+            
         }
     }
 }
